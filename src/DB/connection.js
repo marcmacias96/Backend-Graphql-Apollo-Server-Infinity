f2621b7a7cbbb5af6af6ec215e1292b8cb8c2fdd
marcmacias96 <mamacias3@espe.edu.ec>
2019-07-13 17:32:41 -0500

    funcionalidades terminadas y base de datos cambiados

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/graphql-mongo', { useNewUrlParser: true })
+//mongodb://localhost/graphql-mongo   
+mongoose.connect('mongodb+srv://marcmacias96:Guitarra2896@@cluster0-olbfz.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 .then(() => console.log('connected to db'))
 .catch(err => console.log(err));
