6ab69b6c51ecdab687a1886df7541a9a2d35f7c2
Marco Macias <mamacias3@espe.edu.ec>
2021-06-25 15:34:46 -0500

    Update connection.js

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 //mongodb://localhost/graphql-mongo   
-mongoose.connect('mongodb+srv://marcmacias96:Guitarra2896@@cluster0-olbfz.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://SECRET:HOST', { useNewUrlParser: true })
 .then(() => console.log('connected to db'))
 .catch(err => console.log(err));
